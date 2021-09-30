a93dc077cfa099506b69e8582b6e5681c69031ca
HarshithaG069 <harshithag069@gmail.com>
2021-09-30 09:03:20 +0530

    day6

---
+++ src/com/company/TicTacToeGame.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        char[] board = new char[10];
+        char ch = '1';
+        for (int i = 0; i < 10; i++) {
+               board[i]=' ';
+                board[i] = ch++;
+                break;
+            }
+
+    }
+}
